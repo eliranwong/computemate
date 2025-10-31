4789462bda91b835055136d20cda53867e0b35bd
eliranwong <hkwebtech@gmail.com>
2025-10-31 21:15:01 +0000

    use latest agentmake library

--- package/setup.py
+++ package/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from setuptools.command.install import install
 import os, shutil, platform, sys
 
-version = "0.2.47"
+version = "0.2.48"
 with open(os.path.join("computemate", "version.txt"), "w", encoding="utf-8") as fileObj:
     fileObj.write(version)
 
